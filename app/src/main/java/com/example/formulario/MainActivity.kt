553125542d5b7d59061f2fe1a7af2e4e7ad6153f
Franco Pecci <fapeccisan@gmail.com>
2023-07-28 11:27:41 -0400

    Editando spinners

--- app/src/main/java/com/example/formulario/MainActivity.kt
+++ app/src/main/java/com/example/formulario/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.formulario
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.statusBarColor = Color.parseColor("#800000")
+
+        val spinnersex = findViewById<Spinner>(R.id.sex_edit)
+
+        val listsex = resources.getStringArray(R.array.sex_options)
+
+        val adaptersex = ArrayAdapter<Any?>(this, R.layout.spinner_list, listsex)
+        adaptersex.setDropDownViewResource(R.layout.spinner_list)
+
+        spinnersex.adapter = adaptersex
+
+        val spinnerstatus = findViewById<Spinner>(R.id.civil_status_edit)
+
+        val liststatus = resources.getStringArray(R.array.civil_status_options)
+
+        val adapterstatus = ArrayAdapter<Any?>(this, R.layout.spinner_list, liststatus)
+        adapterstatus.setDropDownViewResource(R.layout.spinner_list)
+
+        spinnerstatus.adapter = adapterstatus
     }
 }
