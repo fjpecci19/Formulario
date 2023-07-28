b28994ed1026d557ca8e733ac0bdafcd3897e963
Franco Pecci <fapeccisan@gmail.com>
2023-07-28 10:37:26 -0400

    Varios

--- app/src/main/java/com/example/formulario/MainActivity.kt
+++ app/src/main/java/com/example/formulario/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.formulario
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.statusBarColor = Color.parseColor("#800000")
     }
 }
